179b6435f6b728951ad7ecfe1dc0e3f1b66febbf
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-03-30 17:58:07 +0600

    event handler in react

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,18 @@ function App() {
   function handleClick(){
     alert("I am clicked");
   }
+
+  const handleAdd3 = (num) => {
+    const newNum = num + 5; 
+    alert(newNum)
+  }
   
   return (
     <>
       <h2>hello react</h2>
       <button onClick={handleClick}>Click me</button>
+      <button onClick={() => alert("click 2")}>Click me 2</button>
+      <button onClick={() => handleAdd3(7)}>click add 3</button>
     </>
   )
 }
